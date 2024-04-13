91116ed4523f4e43ad7ec6a2d71e21f2a870e52f
Himanshu <hr644878@gmail.com>
2024-04-13 20:23:57 +0530

    created fully risponsive navbar

--- 02--MAIN_Project/client/src/App.jsx
+++ 02--MAIN_Project/client/src/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
-      <Router>
+      <Header />
+      <Routes>
         r<Route path="/" element={<Home />} />
         r<Route path="/about" element={<About />} />
         r<Route path="/dashboard" element={<Dashboard />} />
         r<Route path="/projects" element={<Projects />} />
         r<Route path="/sign-in" element={<SignIn />} />
         r<Route path="/sign-up" element={<SignUp />} />
-      </Router>
+      </Routes>
     </BrowserRouter>
   );
 };
---
+++ 02--MAIN_Project/client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { Button, Navbar, NavbarToggle, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FaMoon } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const Header = () => {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2">
+      <Link to={"/"} className="font-bold text-sm sm:text-lg dark:text-white">
+        <span className=" bg-gradient-to-r from-indigo-500 via-blue-500 to-pink-500 text-white px-2 py-1  rounded-md">
+          {" "}
+          HR's{" "}
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="search"
+          className="lg:inline hidden "
+          rightIcon={AiOutlineSearch}
+        />
+      </form>
+      <div className="flex gap-4 md:order-2">
+        <Button color={"gray"} pill className="md:hidden ">
+          <AiOutlineSearch />
+        </Button>
+        <Button color={"gray"} pill className="self-center hidden sm:inline">
+          <FaMoon />
+        </Button>
+        <Link to={"sign-in"}>
+          <Button color={"gray"}>Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link as={"div"} active={path === "/"}>
+          <Link to={"/"}>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={path === "/about"}>
+          <Link to={"/about"}>About</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={path === "/projects"}>
+          <Link to={"/projects"}>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
