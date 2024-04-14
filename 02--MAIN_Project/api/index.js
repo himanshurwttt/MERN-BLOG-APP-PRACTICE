34ee8f18eae00b90ffefa38c2d79052d0e7b9e14
Himanshu <hr644878@gmail.com>
2024-04-14 21:58:24 +0530

    created a test api route

--- 02--MAIN_Project/api/index.js
+++ 02--MAIN_Project/api/index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+
 const app = express();
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log(`we are connected to the MONGODB database`);
+    console.log(`DATABASE connected`);
   })
   .catch((err) => {
     console.log(err);
   });
+
 app.listen(3000, () => {
   console.log(`the server site is started successfully`);
 });
+
+app.use("/api/user", userRoute);
