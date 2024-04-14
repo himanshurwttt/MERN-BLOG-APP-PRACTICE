f3f2ca6c3232756198fe5b58bbde56bc643292bd
Himanshu <hr644878@gmail.com>
2024-04-14 13:01:19 +0530

    connected to the database

--- 02--MAIN_Project/api/index.js
+++ 02--MAIN_Project/api/index.js
@@ -1,6 +1,16 @@
 import e from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = e();
-
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`we are connected to the MONGODB database`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log(`the server site is started successfully`);
 });
