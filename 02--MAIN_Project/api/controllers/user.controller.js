eed5785f95f80fe907d2b11050fa56d53c5da7a9
Himanshu <hr644878@gmail.com>
2024-05-02 23:26:55 +0530

    created update user API route

--- 02--MAIN_Project/api/controllers/user.controller.js
+++ 02--MAIN_Project/api/controllers/user.controller.js
@@ -1,7 +1,62 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ message: `this is test api` });
 };
 
 export const updateUser = async (req, res, next) => {
-  console.log("this si the user", req.user);
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return errorHandler(
+        400,
+        "Passowd must be at least more then 6 characters"
+      );
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username < 7 || req.body.username > 20) {
+      return next(
+        errorHandler(
+          400,
+          "The usename must the more then 7 and less then 20 characters"
+        )
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "The username only contains Lowercases "));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(
+          400,
+          "The username only contains the letter and number (not special charecters)"
+        )
+      );
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            profilePicture: req.body.profilePicture,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {}
+  }
 };
