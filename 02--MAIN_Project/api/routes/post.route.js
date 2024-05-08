075580acb0f0ddb1655b6ceb8f8ae9b061ab59ed
Himanshu <hr644878@gmail.com>
2024-05-08 23:56:40 +0530

    created "getPost" API route

--- 02--MAIN_Project/api/routes/post.route.js
+++ 02--MAIN_Project/api/routes/post.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createPost);
-
+router.get("/getposts", getPosts);
 export default router;
