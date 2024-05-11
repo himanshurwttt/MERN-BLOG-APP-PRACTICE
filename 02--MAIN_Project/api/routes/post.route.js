ed09214abb670c81c36a9a69a0421ad031285755
Himanshu <hr644878@gmail.com>
2024-05-11 22:01:44 +0530

    complete delete post functionality

--- 02--MAIN_Project/api/routes/post.route.js
+++ 02--MAIN_Project/api/routes/post.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import {
+  createPost,
+  deletepost,
+  getPosts,
+} from "../controllers/post.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createPost);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyUser, deletepost);
+
 export default router;
--- 02--MAIN_Project/client/src/components/DashPosts.jsx
+++ 02--MAIN_Project/client/src/components/DashPosts.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Button, Table, TableHead } from "flowbite-react";
+import { Button, Modal, Table } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPost, setUserPost] = useState([{}]);
   const [showMore, setShowMore] = useState(true);
+  const [showModel, setShowModel] = useState(false);
+  const [postIdToDelete, setPostIdToDelete] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -38,6 +43,35 @@ const DashPosts = () => {
         setUserPost((prev) => [...prev, ...data.posts]);
         if (data.posts.length <= 9) {
           setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeletePost = async () => {
+    console.log("contured delete functionality");
+    setShowModel(false);
+    try {
+      const res = await fetch(
+        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      console.log("api called");
+      const data = await res.json();
+      console.log("delete started");
+      if (!res.ok) {
+        console.log(data.message);
+        console.log("deleting canceled");
+      } else {
+        setUserPost((prev) =>
+          prev.filter((post) => post._id !== postIdToDelete)
+        );
+        if (res.ok) {
+          console.log("delete done");
         }
       }
     } catch (error) {
@@ -85,7 +119,13 @@ const DashPosts = () => {
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span className="font-bold md:font-medium hover:underline dark:text-red-500  text-red-600 cursor-pointer">
+                    <span
+                      onClick={() => {
+                        setShowModel(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                      className="font-bold md:font-medium hover:underline dark:text-red-500  text-red-600 cursor-pointer"
+                    >
                       Delete
                     </span>
                   </Table.Cell>
@@ -113,6 +153,30 @@ const DashPosts = () => {
       ) : (
         <p>You have no post created yet </p>
       )}
+      <Modal
+        show={showModel}
+        popup
+        size={"md"}
+        onClose={() => setShowModel(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="w-14 h-14 mx-auto  text-gray-600 dark:text-gray-200 mb-4" />
+            <h3 className="text-lg mx-1 font-semibold mb-6 text-gray-700 dark:text-gray-300">
+              Are you sure you want to delete your Account !
+            </h3>
+            <div className="flex justify-center gap-8 my-2">
+              <Button color={"success"} onClick={handleDeletePost}>
+                Yes i'm sure
+              </Button>
+              <Button color={"failure"} onClick={() => setShowModel(false)}>
+                No, cancel{" "}
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
