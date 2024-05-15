b001bb3ffb98616e4dc0e2a339c7482bd694d20b
Himanshu <hr644878@gmail.com>
2024-05-15 13:00:51 +0530

    .

--- 02--MAIN_Project/client/src/components/DashUsers.jsx
+++ 02--MAIN_Project/client/src/components/DashUsers.jsx
@@ -62,13 +62,9 @@ const DashUsers = () => {
         setShowModel(false);
         console.log("user deleted", userIdToDelete);
       }
-<<<<<<< HEAD
     } catch (error) {
       console.log(error);
     }
-=======
-    } catch (error) {}
->>>>>>> refs/remotes/origin/main
   };
 
   return (
