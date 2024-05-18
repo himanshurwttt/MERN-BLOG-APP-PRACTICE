05badb07f242deb232b317feca07f43eaff6e320
Himanshu <hr644878@gmail.com>
2024-05-18 13:43:35 +0530

    ADDED like functionality in comments

--- 02--MAIN_Project/client/src/components/CommentSection.jsx
+++ 02--MAIN_Project/client/src/components/CommentSection.jsx
@@ -1,13 +1,14 @@
 import { Button, Textarea } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+  const Navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -62,6 +63,34 @@ export default function CommentSection({ postId }) {
     };
     getComments();
   }, [postId]);
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        Navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComment(
+          comments.map((comment) => {
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment;
+          })
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3 ">
@@ -121,7 +150,7 @@ export default function CommentSection({ postId }) {
         <>
           <div className="text-sm my-5 flex items-center gap-1">
             <p>Comments</p>
-            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+            <div className={`border border-gray-400 py-1 px-2 rounded-sm `}>
               <p>{comments.length}</p>
             </div>
           </div>
@@ -129,7 +158,7 @@ export default function CommentSection({ postId }) {
             <Comment
               key={comment._id}
               comment={comment}
-              // onLike={handleLike}
+              onLike={handleLike}
               // onEdit={handleEdit}
               // onDelete={(commentId) => {
               //   setShowModal(true);
