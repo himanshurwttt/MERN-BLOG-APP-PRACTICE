1dfee44bdcdad584b91c72bcb8775319a03df0ba
Himanshu <hr644878@gmail.com>
2024-06-12 00:39:44 +0530

    comment card ui

--- 01--BLOG_APP_(self-created)/client/src/components/Comment.jsx
+++ 01--BLOG_APP_(self-created)/client/src/components/Comment.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { IoMdCreate } from "react-icons/io";
+import { BiTimeFive } from "react-icons/bi";
 
 export default function Comment() {
-  return <div>no comments yet</div>;
+  return (
+    <div className="bg-blue-50 shadow-slate-400 shadow-md outline-1 outline-blue-200  m-auto my-8 p-3 rounded-md md:max-w-md md:mx-auto w-full max-w-md ">
+      <div className=" mb-2 w-full flex flex-row justify-between items-center">
+        <div className="flex items-center gap-1">
+          <img src="" alt="" className="bg-blue-300 w-7 h-7 rounded-full " />{" "}
+          <label className="text-sm italic text-zinc-700">@username</label>
+        </div>
+        <div className="ago text-xs flex flex-row justify-center items-center gap-1">
+          <BiTimeFive className="scale-[1.2] text-zinc-700" /> 3 days ago
+        </div>
+      </div>
+      <hr />
+      <div className="content text-xs text-zinc-600 font-[500] my-4 selection:bg-none">
+        is there something that i can tell you but i wanna know if i can tell
+        you or not bcz i think ican but my mind says i can't what's this
+        difficulties even i don't know what is going on with me is this the end
+        saiyan goku are you really finished or now vegita will continue your
+        legacy btw i like vegita
+      </div>
+      <div className=" p-2 flex justify-between">
+        <div className="flex gap-2 ">
+          <FaRegHeart className=" cursor-pointer active:scale-[0.95] scale-[1.1] duration-100 " />
+          <IoMdCreate className=" cursor-pointer active:scale-[0.95] scale-[1.1] duration-100 " />
+        </div>
+        <div className="text-xs text-zinc-700 selection:bg-none">
+          {" "}
+          Likes: 200
+        </div>
+      </div>
+    </div>
+  );
 }
--- 01--BLOG_APP_(self-created)/client/src/components/CommentSection.jsx
+++ 01--BLOG_APP_(self-created)/client/src/components/CommentSection.jsx
@@ -37,44 +37,49 @@ export default function CommentSection({ postId }) {
   };
 
   return (
-    <div className=" h-80 max-w-lg m-auto">
-      <h1 className="text-center text-5xl uppercase font-[500] text-zinc-800 mb-10">
-        Comments
-      </h1>
-      <div className="flex flex-col  w-full items-end justify-center mt-5">
-        <form
-          onSubmit={handleSubmit}
-          className="w-full px-16 flex flex-col  gap-2 "
-        >
-          <textarea
-            type="text"
-            maxLength="300"
-            className="m-auto w-full h-28  text-zinc-600 font-[500] p-3 italic  focus:outline-none text-xs rounded-md resize-none hover:shadow-2xl duration-200 hover:shadow-blue-300 shadow-lg"
-            placeholder="Write a comment here.."
-            onChange={(e) => setComment(e.target.value)}
-            value={comment}
-            required
-          />
+    <>
+      <div className=" h-full max-w-lg m-auto my-3">
+        <h1 className="text-center text-5xl uppercase font-[500] text-zinc-800 mb-10">
+          Comments
+        </h1>
+        <div className="flex flex-col  w-full items-end justify-center mt-5">
+          <form
+            onSubmit={handleSubmit}
+            className="w-full md:px-8 px-5 flex flex-col  gap-2 "
+          >
+            <textarea
+              type="text"
+              maxLength="300"
+              className="m-auto w-full h-28  text-zinc-600 font-[500] p-3 italic  focus:outline-none text-xs rounded-md resize-none hover:shadow-2xl duration-200 hover:shadow-blue-300 shadow-lg"
+              placeholder="Write a comment here.."
+              onChange={(e) => setComment(e.target.value)}
+              value={comment}
+              required
+            />
 
-          <div className="flex flex-row justify-between items-center">
-            <p className="text-xs text-zinc-600 italic font-[400]">
-              Character Remaning: {300 - comment.length}
-            </p>
-            <button
-              onClick={handleSubmit}
-              className="hover:bg-green-400 duration-200   self-end shadow-lg border-green-300 bg-green-200 w-20 h-9 text-sm font-[500] rounded-md scale-[1] active:scale-[0.90]"
-            >
-              Submit
-            </button>
-          </div>
-          {formError && (
-            <p className="text-xs text-red-600 italic font-[400] bg-red-200 p-1 rounded-md w-full">
-              {formError}
-            </p>
-          )}
-        </form>
+            <div className="flex flex-row justify-between items-center">
+              <p className="text-xs text-zinc-600 italic font-[400]">
+                Character Remaning: {300 - comment.length}
+              </p>
+              <button
+                onClick={handleSubmit}
+                className="hover:bg-green-400 duration-200   self-end shadow-lg border-green-300 bg-green-200 w-20 h-9 text-sm font-[500] rounded-md scale-[1] active:scale-[0.90]"
+              >
+                Submit
+              </button>
+            </div>
+            {formError && (
+              <p className="text-xs text-red-600 italic font-[400] bg-red-200 p-1 rounded-md w-full">
+                {formError}
+              </p>
+            )}
+          </form>
+        </div>
       </div>
-      <Comment />
-    </div>
+      <div className="w-full md:px-8 px-5 flex flex-col">
+        <Comment />
+        <Comment />
+      </div>
+    </>
   );
 }
